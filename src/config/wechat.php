22baa342e789d9602299e800235db15923e8ad03
fermat <790267810@qq.com>
2015-02-27 21:37:06 +0800

    Update and rename src to src/config/wechat.php

---
+++ src/config/wechat.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'default' => array(
+        'token'          => 'token',//填写应用接口的Token
+        'encodingaeskey' => 'encodingaeskey',//填写加密用的EncodingAESKey
+        'appid'          => 'appid',//填写高级调用功能的app id
+        'appsecret'      => 'appsecret',//填写高级调用功能的密钥
+		'agentid'		 =>'agentid', //应用id   AgentID
+        'debug'          => false,//调试开关
+        'logcallback'    => false,//调试输出方法开关
+    )
+);

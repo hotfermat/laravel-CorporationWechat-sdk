b16550bc545c6eb5017bce3b0ecb870cba76d350
fermat <790267810@qq.com>
2015-02-27 21:34:00 +0800

    Create Wechat.php

---
+++ src/xxy/Wechat/Facades/Wechat.php
@@ -0,0 +1,9 @@
+<?php  namespace xxy\Wechat\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Wechat extends Facade {
+
+    protected static function getFacadeAccessor() { return 'wechat'; }
+
+}
